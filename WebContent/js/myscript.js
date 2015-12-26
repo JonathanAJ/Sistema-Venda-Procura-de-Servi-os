1d2cab4084c627c3c35c519b236a5bbece85c674
Jonathan Alves <alves_aj@windowslive.com>
2015-12-26 14:40:44 -0300

    pegando login e senha do form

--- WebContent/js/myscript.js
+++ WebContent/js/myscript.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-	// Preparando modal
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	// Inicializando modal Materialize
     $('.modal-trigger').leanModal();
     
-    //Login
+    //Fazendo login
     $("#fazerLogin").click(function(){
     	var bt = $("#fazerLogin");
     	if(!bt.hasClass('disabled')){
         	bt.addClass("disabled");
+        	var senha = $('#senha').val();
+        	var login = $('#login').val();
         	$.ajax({
     		  method: "POST",
     		  url: "Login",
-    		  data: {login: "JonathanA", senha: "12345" },
+    		  data: {login: login, senha: senha},
     		  beforeSend: function() {
     			  
     		  }
